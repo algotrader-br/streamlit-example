3c497a524029b161ee45a15e61140a0c5e8d526f
algotraderbr <algotraderbr@gmail.com>
2023-09-05 18:00:07 -0300

    updt backtest

--- pages/3_Backtest_janela_longa.py
+++ pages/3_Backtest_janela_longa.py
@@ -30,3 +30,67 @@ st.markdown('''
 #################################
 ###  0. Data Prep  ###
 #################################
+
+# Importando backtest
+df = pd.read_csv('bases/sol_6_5m_full.csv', index_col=['time'], parse_dates=['time'])
+df = df.dropna()
+
+
+#################################
+###  1. Plot Resultado  ###
+#################################
+
+
+figback = go.Figure()
+figback.add_trace(go.Scatter(
+    x=df.index,
+    y=df["cstrategy"],
+    name='Backtest'
+))
+
+# adicionando elementos de layout
+figback.update_layout(
+    title = dict(text="1. Retorno Acumulado", font=dict(size=27), automargin=False, yref='paper'),
+    xaxis_title= dict(text="<b> Data </b>", font=dict(size=20)),
+    yaxis_title= dict(text="<b>Retorno (R$) </b>", font=dict(size=20)),
+    font_family="Arial",
+    font_color="black",
+    title_font_family="Arial",
+    title_font_color="black",
+    legend_title_font_color="green",
+    showlegend=True,
+    autosize=False,
+    width=800,
+    height=500,
+    
+    xaxis=dict(
+        showline=True,
+        showgrid=True,
+        showticklabels=True,
+        linecolor='white',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Arial',
+            size=15,
+            color='black',
+        ),
+    ),
+    yaxis=dict(
+        showline=True,
+        showgrid=True,
+        showticklabels=True,
+        linecolor='white',
+        linewidth=2,
+        ticks='outside',
+        tickfont=dict(
+            family='Arial',
+            size=15,
+            color='black',
+        ),
+    )
+)
+
+# Plot!
+figback.update_layout(legend_title_text='Legend')
+st.plotly_chart(figback, use_container_width=True)
