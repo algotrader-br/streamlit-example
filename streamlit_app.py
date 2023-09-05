1d573a962ee64901530103cc685811272b743a6d
algotraderbr <algotraderbr@gmail.com>
2023-09-05 12:27:33 -0300

    2nd commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,39 @@
-from collections import namedtuple
-import altair as alt
-import math
+#################################
+### Importando as bibliotecas ###
+#################################
+
+import numpy as np
+import datetime as dt
 import pandas as pd
 import streamlit as st
 
+st.write("# Bem-vindo à página de resultados do AlgoTrader BR! 👋")
+st.sidebar.success("Selecione a página acima")
+
+st.markdown(
+    """
+    ## Introdução
+
+    Nesta página você poderá encontrar os resultados dos meus trades diários em
+    **conta real**! A ideia é que a atualização seja quase em *real-time*.     
+
+    Atualmente meus sistemas de **algo trading** são especializados em *day trade* para o mini-indice (**WINFUT**), onde
+    os trades são executados de forma **100% automatizadas** utilizando o MetaTrader 5.
+
+   👈 Selecione a página ao lado para obter as informações que você deseja.
+    Seja informações de **performance** (backtest e/ou real), **estatísticas dos trades** (e.g. ganhos/perdas),
+    e também o detalhamento das **diferenças de backtest e conta real**.
+
+    ### Quer aprender mais?
+
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+      forums](https://discuss.streamlit.io)
+
+    ### Contato
+
+    - Dúvidas e sugestões, por favor entrar em contato pelo e-mail: algotraderbr@gmail.com
+
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+)
